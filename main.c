3583a3b103fe455f434494f3ca039c0e25790bfb
Aminebouramtane <bouramtaneamine1@gmail.com>
2024-05-31 17:58:59 +0100

    syntax err

--- main.c
+++ main.c
@@ -37,6 +37,8 @@ int main(int ac, char **av)
         lexer(input, data);
         ft_expand(data->ndata);
         ft_parce(&parce, data);
+        if (syntax_err(data->ndata))
+            continue;
         while (data->ndata)
 	    {
             printf("cmd ## %s state ## %d type ## %c\n", data->ndata->cmd, data->ndata->state, data->ndata->type);
