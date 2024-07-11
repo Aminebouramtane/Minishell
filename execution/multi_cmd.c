78f21bb76715ea78d4f682f10e6075b4623abdbe
yassin <yassineimizare@gmail.com>
2024-07-11 15:54:34 +0100

    append

--- execution/multi_cmd.c
+++ execution/multi_cmd.c
@@ -5,12 +5,25 @@ void	execute_multi(t_parce_node *parce, char **envp)
 	(void)parce;
 	(void)envp;
 	int	pid;
+	int fd[2];
 
-	pid = 0;
 	while (parce != NULL)
 	{
+		pipe(fd);
 		pid = fork();
-		parce = parce->next;
+		if (pid == 0)
+		{
+			while (parce->file)
+			{
+				if (parce->file->redir_in)
+					open_in_files_redir(parce);
+				if (parce->file->redir_out)
+					open_out_files_redir(parce);
+				if (parce->file->append)
+					open_files_append(parce);	
+				parce->file = parce->file->next;
+			}
+		}
+			parce = parce->next;
 	}
-	
 }
