b219688c30ced3461fc66f2b61652bcbd3226054
Amine Bouramtane <bouramtaneamine1@gmail.com>
2024-07-27 12:19:05 +0100

    some changes in expand

--- expand/expand.c
+++ expand/expand.c
@@ -14,15 +14,19 @@ void	ft_expand_dolar(Datatoken *lst)
 	node = lst;
 	while (node)
 	{
-		if (node->cmd && node->cmd[0] == '$' && ft_strlen(node->cmd) == 1 && node->next->cmd
+		if (node->cmd && node->cmd[0] && node->cmd[0] == '$' && ft_strlen(node->cmd) == 1 && node->next && node->next->cmd 
 		&& (node->next->cmd[0] == '\'' || node->next->cmd[0] == '\"'))
 		{
 			tmp = "\0";
 			node->cmd = tmp;
 		}
-		if (node->cmd && node->cmd[0] == '$' && ft_strlen(node->cmd) > 2)
+		if (node->cmd && node->cmd[0] == '$' && ft_strlen(node->cmd) == 1)
 		{
-			node->cmd++;
+			node->cmd = "$";
+		}
+		else if (node->cmd && node->cmd[0] == '$' && ft_strlen(node->cmd) > 2)
+		{
+			node->cmd = node->cmd + 1;
 			str = node->cmd;
 			env = getenv(str);
 			tmp = my_strdup_two(env);
@@ -31,7 +35,7 @@ void	ft_expand_dolar(Datatoken *lst)
 		}
 		else if (node->cmd && node->cmd[0] == '$' && ft_strlen(node->cmd) == 2)
 		{
-			node->cmd++;
+			node->cmd = node->cmd + 1;
 			str = node->cmd;
 			env = getenv("_");
 			if (str[0] == '_')
@@ -62,7 +66,7 @@ void	ft_expand_home(Datatoken *lst)
 	{
 		if (node->cmd[0] == '~' && node->state == 2)
 		{
-			node->cmd++;
+			node->cmd = node->cmd + 1;
 
 			str = node->cmd;
 			env = my_strdup_two(getenv("HOME"));
@@ -76,56 +80,9 @@ void	ft_expand_home(Datatoken *lst)
 	}
 }
 
-void	ft_remove_dqoutes(Datatoken **lst)
-{
-	Datatoken	*node;
-
-	node = *lst;
-	while (node)
-	{
-		if (node->cmd[0] == '\"')
-		{
-			node->cmd++;
-			if (node->prev)
-				node->prev->next = node->next;
-			if (node->next)
-				node->next->prev = node->prev;
-			if (node == *lst)
-				*lst = node->next;
-			node = node->next;
-		}
-		else
-			node = node->next;
-	}
-}
-
-void	ft_remove_qoutes(Datatoken *lst)
-{
-	Datatoken	*node;
-	char		*str;
-	int			i;
-
-	node = lst;
-	i = 0;
-	str = NULL;
-	while (node)
-	{
-		if (node->cmd[0] == '\'' && node->state == 0)
-		{
-			node->cmd++;
-			while (node->cmd[i] != '\'')
-				i++;
-			str = my_strdup(node->cmd, i);
-			node->cmd = str;
-		}
-		node = node->next;
-	}
-}
-
 void	ft_expand(Datatoken *lst)
 {
 	ft_expand_dolar(lst);
 	ft_expand_home(lst);
-	// ft_remove_qoutes(lst);
-	// ft_remove_dqoutes(&lst);
+
 }
