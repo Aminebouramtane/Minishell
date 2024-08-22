b31e4d4963f29e178e875f9620c8f570059ba63a
Aminebouramtane <bouramtaneamine1@gmail.com>
2024-08-22 16:24:33 +0100

    resolved

--- execution/heredoc/ex.c
+++ execution/heredoc/ex.c
@@ -1,74 +1,112 @@
 #include "../../minishell.h"
 
-void	ft_heredoc_expand_dolar(t_heredoc *lst)
+static void	ft_expand_h_dolar_single_char(t_heredoc *node)
+{
+	if (node->input[0] == '$' && ft_strlen(node->input) == 1)
+		node->input = "$";
+}
+
+static void	ft_expand_h_dolar_two_chars(t_heredoc *node)
+{
+	char	*str;
+	char	*env;
+	char	*tmp;
+
+	if (node->input[0] == '$' && ft_strlen(node->input) == 2)
+	{
+		tmp = node->input;
+		node->input += 1;
+		str = node->input;
+		env = getenv("_");
+		if (str[0] == '_' || str[0] == '?')
+		{
+			if (str[0] == '_')
+				node->input = my_strdup_two(env);
+			else if (str[0] == '?')
+				node->input = ft_itoa(envi->exit_status);
+			else
+				node->input = "\0";
+		}
+		else
+			node->input = tmp;
+	}
+}
+
+static void	ft_handle_h_special_case(t_heredoc *node)
+{
+	if (node->input && node->input[0] && node->input[0] == '$'
+		&& ft_strlen(node->input) == 1 && node->next && node->next->input
+		&& (node->next->input[0] == '\'' || node->next->input[0] == '\"'))
+	{
+		node->input = "\0";
+	}
+}
+
+static void	ft_expand_h_dolar_long(t_heredoc *node)
+{
+	char	*str;
+	char	*tmp;
+	char	*env;
+
+	if (node->input[0] == '$' && ft_strlen(node->input) > 2)
+	{
+		if (ft_isdigit(node->input[1]))
+		{
+			node->input = node->input + 2;
+			return ;
+		}
+		node->input += 1;
+		str = node->input;
+		env = getenv(str);
+		tmp = my_strdup_two(env);
+		if (tmp)
+			node->input = tmp;
+		else
+			node->input = "\0";
+	}
+}
+
+static void	ft_expand_h_dolar_single(t_heredoc *node)
+{
+	ft_expand_h_dolar_single_char(node);
+	ft_expand_h_dolar_long(node);
+	ft_expand_h_dolar_two_chars(node);
+}
+
+static void	ft_heredoc_expand_dolar(t_heredoc *lst)
 {
 	t_heredoc	*node;
-	char		*str;
-	char		*tmp;
-	char		*env;
 
-	if (!lst)
-		return ;
 	node = lst;
 	while (node)
 	{
-		if (node->input && node->input[0]
-			&& node->input[0] == '$' && ft_strlen(node->input) == 1)
+		ft_handle_h_special_case(node);
+		if (node->input && node->input[0] == '$')
 		{
-			if (node->next && node->next->input && (node->next->input[0] == '\''
-					|| node->next->input[0] == '\"'))
-			{
-				tmp = "\0";
-				node->input = tmp;
-			}
-		}
-		else if (node->input && node->input[0] == '$'
-			&& ft_strlen(node->input) > 1)
-		{
-			str = node->input + 1;
-			env = getenv(str);
-			tmp = my_strdup_two(env);
-			if (tmp)
-				node->input = tmp;
-		}
-		else if (node->input && node->input[0] == '$'
-			&& ft_strlen(node->input) == 2)
-		{
-			str = node->input + 1;
-			env = getenv("_");
-			if (str && str[0] == '_')
-			{
-				tmp = my_strdup_two(env);
-				if (tmp)
-					node->input = tmp;
-			}
-			else
-			{
-				tmp = "\0";
-				node->input = tmp;
-			}
+			ft_expand_h_dolar_single(node);
 		}
 		node = node->next;
 	}
 }
 
-void	ft_heredoc_expand_home(t_heredoc *lst)
+static void	ft_heredoc_expand_home(t_heredoc *lst)
 {
 	t_heredoc	*node;
 	char		*tmp;
-	char		*str;
 	char		*env;
 
 	node = lst;
 	while (node)
 	{
-		if (node->input && node->input[0] == '~')
+		if (node->input[0] == '~')
 		{
-			str = node->input + 1;
+			node->input += 1;
 			env = my_strdup_two(getenv("HOME"));
-			tmp = ft_strjoin(env, str);
+			tmp = ft_strjoin(env, node->input);
 			if (tmp)
 				node->input = tmp;
+			else
+				node->input--;
 		}
 		node = node->next;
 	}
